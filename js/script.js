dfaa9363131c95cede0b80d636da887347dc6404
Prasad Kalvikatti <65800954+prasad-droid@users.noreply.github.com>
2023-09-17 17:22:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,139 +17,128 @@ function reset() {
 
 //change Page
 async function changePage(n) {
-  try{
-  home.innerHTML = "";
-  pageNo += n;
-  if (pageNo < 1) pageNo = 10;
-  if (pageNo > 10) pageNo = 1;
-  console.log(pageNo);
-  $("#page").html(pageNo);
-  let response = await fetch(`${currentLink}` + pageNo);
+  try {
+    home.innerHTML = "";
+    pageNo += n;
+    if (pageNo < 1) pageNo = 10;
+    if (pageNo > 10) pageNo = 1;
+    console.log(pageNo);
+    $("#page").html(pageNo);
 
-  data = await response.json();
+    console.log(`${currentLink}${pageNo}`);
+    let response = await fetch(`${currentLink}${pageNo}`);
+    data = await response.json();
 
-  data.forEach((ele) => {
-    createCard(ele);
-  });
-
-  console.log(currentLink, pageNo);}
-  catch{
-    home.innerHTML=_404()
+    data.results.forEach((ele) => {
+      createCard(ele);
+    });
+  } catch {
+    home.innerHTML = _404();
   }
 }
 
 // Home Page
 async function homepage() {
-  try
-  {home.innerHTML = "";
-  reset();
-  let response = await fetch(
-    `https://api.consumet.org/anime/gogoanime/recent-episodes`
-  );
+  try {
+    home.innerHTML = "";
+    reset();
+    let response = await fetch(
+      `https://api.consumet.org/anime/gogoanime/recent-episodes?type=1&page=${pageNo}`
+    );
 
-  data = await response.json();
-   console.log(data)
-   
-  data.results.forEach((ele) => {
-    createCard(ele);
-  });
-  currentLink = `https://gogoanime.consumet.stream/recent-release?type=1&page=`;}
-  catch {
-    home.innerHTML=_404()
+    data = await response.json();
+    console.log(data);
+
+    data.results.forEach((ele) => {
+      createCard(ele);
+    });
+    currentLink = `https://api.consumet.org/anime/gogoanime/recent-episodes?type=1&page=`;
+  } catch {
+    console.log(error);
+    home.innerHTML = _404();
   }
 }
-// Popular Page
-async function popularpage() {
- 
- try{ reset();
-  home.innerHTML = "";
-  let response = await fetch(
-    "https://gogoanime.consumet.stream/popular?type=1&page=" + pageNo
-  );
+// Top Airing Page
+async function topAiring() {
+  try {
+    reset();
+    home.innerHTML = "";
+    let response = await fetch(
+      "https://api.consumet.org/anime/gogoanime/top-airing?type=1&page=" +
+        pageNo
+    );
 
-  data = await response.json();
-  data.forEach((ele) => {
-    createCard(ele);
-  });
-  currentLink = `https://gogoanime.consumet.stream/popular?type=1&page=`;}
-  catch{
-    home.innerHTML=_404()
+    data = await response.json();
+    data.results.forEach((ele) => {
+      createCard(ele);
+    });
+    currentLink = `https://api.consumet.org/anime/gogoanime/top-airing?type=1&page=`;
+  } catch {
+    home.innerHTML = _404();
   }
 }
-// Movies page
-async function moviespage() {
-  try{
-  reset();
-  home.innerHTML = "";
-  let response = await fetch(
-    "https://gogoanime.consumet.stream/anime-movies?page=" + pageNo
-  );
 
-  data = await response.json();
-  data.forEach((ele) => {
-    createCard(ele);
-  });
-  currentLink = `https://gogoanime.consumet.stream/anime-movies?page=`;}
-  catch{
-    home.innerHTML=_404()
-  }
-}
 // Recent Page
 async function recentpage() {
-  try {reset();
-  home.innerHTML = "";
-  let response = await fetch(
-    "https://gogoanime.consumet.stream/top-airing?page=" + pageNo
-  );
+  try {
+    reset();
+    home.innerHTML = "";
+    let response = await fetch(
+      `https://api.consumet.org/anime/gogoanime/recent-episodes?type=1&page=${pageNo +1}`
+    );
 
-  data = await response.json();
-  data.forEach((ele) => {
-    createCard(ele);
-  });
-  currentLink = `https://gogoanime.consumet.stream/top-airing?page=`;}
-  catch{
-    home.innerHTML=_404()
+    data = await response.json();
+    data.results.forEach((ele) => {
+      createCard(ele);
+    });
+    currentLink = `https://api.consumet.org/anime/gogoanime/recent-episodes?type=1&page=`;
+  } catch {
+    home.innerHTML = _404();
   }
 }
+
 // search page
 async function searchpage() {
-  try{reset();
-  home.innerHTML = "";
-  let response = await fetch(
-    `https://api.consumet.org/anime/gogoanime/${search.value}&page=` + pageNo
-  );
+  try {
+    reset();
+    home.innerHTML = "";
+    let response = await fetch(
+      `https://api.consumet.org/anime/gogoanime/${search.value}?page=${pageNo}`
+    );
 
-  data = await response.json();
-  data.forEach((ele) => {
-    createCard(ele);
-  });
+    data = await response.json();
+    data.results.forEach((ele) => {
+      createCard(ele);
+    });
 
-  if (data.length==0){
-    console.log("Yes");
-    home.innerHTML=`didn't Find What You are Searching For`
-  }
-  $(".next").hide();
-  $(".prev").hide();
-  $('.pageNo').hide()}
-  catch{
-    home.innerHTML=_404()
+    if (data.length == 0) {
+      console.log("Yes");
+      home.innerHTML = `didn't Find What You are Searching For`;
+    }
+    $(".next").hide();
+    $(".prev").hide();
+    $(".pageNo").hide();
+  } catch {
+    home.innerHTML = _404();
   }
 }
 
 //---------------------------------- category click event handler------------------------------------
 $(".category").click(async function (e) {
-  try{
-  reset();
-  let genre = e.currentTarget.innerHTML.toLowerCase();
-  home.innerHTML = "";
-  let response = await fetch(`https://gogoanime.consumet.stream/genre/${genre}`);
+  try {
+    reset();
+    let genre = e.currentTarget.innerHTML.toLowerCase();
+    home.innerHTML = "";
+    let response = await fetch(
+      `https://gogoanime.consumet.stream/genre/${genre}`
+    );
 
-  data = await response.json();
-  data.forEach((ele) => {
-    createCard(ele);
-  });}
-  catch{
-    home.innerHTML=_404()
+    data = await response.json();
+    data.forEach((ele) => {
+      createCard(ele);
+    });
+  } catch {
+    home.innerHTML = _404();
   }
 });
 
@@ -172,52 +161,55 @@ function createCard(ele) {
     "text-center"
   );
   card.innerHTML = `
-      <img class="card-img-top img-fit" src="${ele.image}" alt="${ele.animeId}" >
+      <img class="card-img-top img-fit" src="${ele.image}" alt="${ele.episodeId}" >
       
                         `;
-  ele.hasOwnProperty("episodeNum")
+  ele.hasOwnProperty("episodeNumber")
     ? //-------------------------------------------Home Page----------------------------------------
       (card.innerHTML += `<div class="card-body ">
-  <h4 class="card-title f-0-8rem">${ele.animeTitle}</h4>
-  <p class="card-text">Eps. ${ele.episodeNum}</p>
+  <h4 class="card-title f-0-8rem">${ele.title}</h4>
+  <p class="card-text">Eps. ${ele.episodeNumber}</p>
   
-  <a href="${ele.episodeUrl}">
+  <a href="${ele.url}">
   </div>`)
-    : ele.hasOwnProperty("releasedDate")
-    ? //----------------------------------------Movies & Popular Page--------------------------------
+    : ele.hasOwnProperty("releaseDate")
+    ? //----------------------------------------Search Page--------------------------------
 
       (card.innerHTML += `<div class="card-body ">
-    <h4 class="card-title f-0-8rem">${ele.animeTitle}</h4>
-    <p class="card-text">Released Date<br> ${ele.releasedDate}</p>
+    <h4 class="card-title f-0-8rem">${ele.title}</h4>
+    <p class="card-text">${ele.releaseDate}</p>
     
-    <a href="${ele.animeUrl}">
+    <a href="${ele.url}">
     </div>`)
     : ele.hasOwnProperty("latestEp")
     ? //-------------------------------------------Recent Page--------------------------------------
 
       (card.innerHTML += `<div class="card-body ">
-    <h4 class="card-title f-0-8rem">${ele.animeTitle}</h4>
+    <h4 class="card-title f-0-8rem">${ele.title}</h4>
     <p class="card-text"> ${ele.latestEp}</p>
     
-    <a href="${ele.animeUrl}">
+    <a href="${ele.url}">
     </div>`)
-    : ele.hasOwnProperty("status")
-    ? //-------------------------------------------Search Page-------------------------------------
-      (card.innerHTML += `<div class="card-body ">
-    <h4 class="card-title f-0-8rem">${ele.animeTitle}</h4>
-    <p class="card-text"> ${ele.status}</p>
-    
-    <a href="${ele.animeUrl}">
-    </div>`)
+    : ele.hasOwnProperty("genres")
+    ? (card.innerHTML += `
+    <div class="card-body ">
+    <h4 class="card-title f-0-8rem">${ele.title}</h4>
+    <p class="card-text"> 
+    <span class="text-dark badge bg-warning category">${ele.genres[0]}</span>
+    <span class="text-dark badge bg-warning category">${ele.genres[1]}</span>
+    <span class="text-dark badge bg-warning category">${ele.genres[2]}</span>
+    </p>    
+    <a href="${ele.url}">
+    </div>
+    `)
     : console.log("error");
 
   // var variable = (condition) ? (true block) : ((condition2) ? (true block2) : (else block2))
   home.appendChild(card);
 }
 
-function _404(){
-  return (
-    `
+function _404() {
+  return `
     <div class="error-page">
     <h1>404</h1>
     <h2>Page Not Found</h2>
@@ -227,8 +219,7 @@ function _404(){
 
   </div>
 <style>
-    `
-  )
+    `;
 }
 
 home.addEventListener("load", homepage());
